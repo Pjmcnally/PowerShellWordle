f99b9341086aa24895f030708b5e8da66a8944d4
aloushin01 <amyloushin@gmail.com>
2023-01-10 20:46:06 -0600

    Add GuessWords data to module

--- PowerShellWordle/PowerShellWordle.psm1
+++ PowerShellWordle/PowerShellWordle.psm1
@@ -20,7 +20,9 @@ ForEach($import in @($Classes + $Public + $Private))
     }
 }
  
+# Variables containing words referenced across the module
 $answerWords = Get-Content -Path "$PSScriptRoot\Data\AnswerWords.txt"
+$guessWords = [System.Collections.Generic.HashSet[string]]@(Get-Content -Path "$PSScriptRoot\Data\GuessWords.txt")
 
 # Here I might...
 # Read in or create an initial config file and variable
