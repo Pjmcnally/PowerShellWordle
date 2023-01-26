629a385a2604ed29f1871885a53b56787c87eeb0
aloushin01 <amyloushin@gmail.com>
2023-01-25 20:13:41 -0600

    Add TODOs to Read-Guess function

--- PowerShellWordle/Private/Read-Guess.ps1
+++ PowerShellWordle/Private/Read-Guess.ps1
@@ -8,6 +8,8 @@ function Read-Guess {
             $guess = $null
         } elseif (-not $guessWords.contains($guess)) {
             # TODO: Overwrite the previous line instead of adding a new line to the terminal.
+            # TODO: Add feedback for why guess is invalid (ie contains number or symbols or too many letters)
+            # TODO: prevent user from making same guess twice
             $guess = Read-Host "Sorry, that guess is invalid. Please try again (or enter 'quit' to quit)"
         }
     }
